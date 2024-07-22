92a4c0f77892a218a9b1f732bc05a28d2e4291b0
OfficialPapu <papuyadavofficial386@gmail.com>
2024-07-23 01:18:08 +0545

    Fixed dns error

--- Account/UserAccount/DSN Point.php
+++ Account/UserAccount/DSN Point.php
@@ -61,9 +61,9 @@ include_once $base_url . 'Assets/Components/Left Navbar.php';
         <h1 class="text-xl md:text-2xl font-bold dsn-heading mb-2 text-[#ff007f]">
           Total DS Points
         </h1>
-        <div class="text-3xl md:text-4xl font-bold text-primary dsn-points text-[#00adef]"><?php echo $dsnPoint; ?></div>
+        <div class="text-3xl md:text-4xl font-bold text-primary dsn-points text-[#00adef]"><?php echo $DsnPoint; ?></div>
         <div class="m-2 text-sm md:text-base text-gray-500 dark:text-gray-400">
-          You have collected <?php echo $dsnPoint; ?> points so far.
+          You have collected <?php echo $DsnPoint; ?> points so far.
         </div>
         <div class="mt-3 flex md:flex-row gap-4">
           <a class="inline-flex items-center justify-center px-4 py-2 border bg-[#FF007F] hover:bg-[#00ADEF] transition duration-300 text-white text-sm font-medium bg-primary rounded-md hover:bg-primary-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus-visible:ring-offset-gray-800" href="Account/UserAccount/My Orders.php" rel="ugc">
