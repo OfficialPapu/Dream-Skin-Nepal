025443e2995d8d7dae155a2f2354d8828b92209e
OfficialPapu <papuyadavofficial386@gmail.com>
2024-07-24 10:45:35 +0545

    Offer Time Updated

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,24 +1,3 @@
-let countDownTime = new Date('July 15, 2024 23:00:00').getTime();
-setInterval(() => {
-    let today = new Date().getTime();
-    let difference = countDownTime - today;
-    
-    if (difference > 0) {
-        let hours = Math.floor(difference / (1000 * 60 * 60));
-        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        
-        $('.hour').html(hours + `<span class="time-info"> hrs</span>`);
-        $('.minute').html(minutes + `<span class="time-info"> min</span>`);
-        $('.second').html(seconds + `<span class="time-info"> sec</span>`);
-        
-    } else {
-        $('.hour').html('00<span class="time-info"> hrs</span>');
-        $('.minute').html('00<span class="time-info"> min</span>');
-        $('.second').html('00<span class="time-info"> sec</span>');
-    }
-}, 1000);
-
-    setTimeout(function() {
-        document.querySelector('.whatsapp').classList.add('show-whatsapp');
-    }, 2000);
+setTimeout(function () {
+    document.querySelector('.whatsapp').classList.add('show-whatsapp');
+}, 2000);
--- index.php
+++ index.php
@@ -158,6 +158,28 @@ include 'Assets/PHP/URL/Base Path.php';
 
 </body>
 <script src="Assets/JS/Script.js"></script>
+<script>
+    let countDownTime = new Date('July 26, 2024 23:00:00').getTime();
+    setInterval(() => {
+        let today = new Date().getTime();
+        let difference = countDownTime - today;
+
+        if (difference > 0) {
+            let hours = Math.floor(difference / (1000 * 60 * 60));
+            let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+            $('.hour').html(hours + `<span class="time-info"> hrs</span>`);
+            $('.minute').html(minutes + `<span class="time-info"> min</span>`);
+            $('.second').html(seconds + `<span class="time-info"> sec</span>`);
+
+        } else {
+            $('.hour').html('00<span class="time-info"> hrs</span>');
+            $('.minute').html('00<span class="time-info"> min</span>');
+            $('.second').html('00<span class="time-info"> sec</span>');
+        }
+    }, 1000);
+</script>
 <script type="application/ld+json">
     {
         "@context": "https://schema.org",
